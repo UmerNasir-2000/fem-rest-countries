281f9f63fad8227ae63acd795f46908c44f8a4a0
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-01-28 20:23:04 +0500

    feat: add country data fetching to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,17 @@
+import { useQuery } from "@tanstack/react-query"
+
+async function fetchCountries() {
+  const response = await fetch(`https://restcountries.com/v3.1/all`)
+  return response.json()
+}
 
 const HomePage: React.FC = () => {
-  return <h1>Home Page</h1>
+  const { data } = useQuery({
+    queryKey: ["countries"],
+    queryFn: fetchCountries,
+  })
+
+  return <h1>{JSON.stringify(data)}</h1>
 }
 
 export default HomePage
